5e9d70dd4cb8dc7ba3a1d701e21c484f6d566e65
Alexander L. Plata <platadevcode@gmail.com>
2025-12-20 23:48:11 -0600

    feat(domain): enforce credit invariants in customer model

--- src/customer/domain/value-objects/AvailableCredit.ts
+++ src/customer/domain/value-objects/AvailableCredit.ts
@@ -7,19 +7,23 @@ export class AvailableCredit {
     this.balance = balance;
   }
 
+  public get value(): Money {
+    return this.balance;
+  }
+
   public static zero(): AvailableCredit {
     return new AvailableCredit(Money.zero());
   }
 
   public increase(amount: Money): AvailableCredit {
-    if (amount.isPositive())
+    if (!amount.isPositive())
       throw new Error('Amount to increase must positive.');
     const newBalance = this.balance.add(amount);
     return new AvailableCredit(newBalance);
   }
 
   public decrease(amount: Money) {
-    if (amount.isPositive())
+    if (!amount.isPositive())
       throw new Error('Amount to increase must positive.');
     if (amount.isGreaterThan(this.balance))
       throw new Error('Insufficient available credit.');
